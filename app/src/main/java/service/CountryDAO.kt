03a85624e3b570fda74e854c541e74dda11aa8bd
Barış Aslankan <barisaslankan@gmail.com>
2023-12-02 14:31:59 +0300

    Correct dependencies

--- app/src/main/java/service/CountryDAO.kt
+++ app/src/main/java/service/CountryDAO.kt
@@ -1,9 +1,11 @@
 package service
 
+import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.Query
 import model.Country
 
+@Dao
 interface CountryDAO {
 
     @Query("SELECT * FROM country")
